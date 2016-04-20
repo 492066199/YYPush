012a2172fef2861eec8a7e47c4f8da9b6030a259
492066199@qq.com <492066199@qq.com>
2016-04-20 11:11:46 +0800

    change zookeeper

--- src/main/java/com/sailing/zookeeper/ZkFactory.java
+++ src/main/java/com/sailing/zookeeper/ZkFactory.java
@@ -4,7 +4,7 @@ package com.sailing.zookeeper;
 public class ZkFactory {
 	private static volatile boolean expare = false;
 	private static volatile ZkConfig zkConfig = null;
-	private final static String connectStr = "10.77.96.122:2181"; 
+	private final static String connectStr = "172.16.89.130:2181,172.16.89.128:2181,172.16.89.129:2181"; 
 	
 	public static ZkConfig getZkConfig(){		
 		if(zkConfig == null){
