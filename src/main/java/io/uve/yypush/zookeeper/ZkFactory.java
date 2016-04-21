03b108be8b6d4f94a96c2c91d04f41a9868ef952
492066199@qq.com <492066199@qq.com>
2016-04-21 15:06:43 +0800

    add env

--- src/main/java/io/uve/yypush/zookeeper/ZkFactory.java
+++ src/main/java/io/uve/yypush/zookeeper/ZkFactory.java
@@ -8,7 +8,16 @@ package io.uve.yypush.zookeeper;
 public class ZkFactory {
 	private static volatile boolean expare = false;
 	private static volatile ZkConfig zkConfig = null;
-	private final static String connectStr = "172.16.89.130:2181,172.16.89.128:2181,172.16.89.129:2181"; 
+	private final static String connectStr; 
+	
+	static {
+		String zookeeper = System.getenv("zookeeper");
+		if(zookeeper == null){
+			connectStr	= "172.16.89.130:2181,172.16.89.128:2181,172.16.89.129:2181";
+		}else {
+			connectStr = zookeeper;
+		}
+	}
 	
 	public static ZkConfig getZkConfig(){		
 		if(zkConfig == null){
